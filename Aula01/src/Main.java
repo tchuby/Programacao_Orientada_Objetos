a763668a5054ebc34164707fcebd26e1c09f77b4
DANIEL ANDRADE SILVA <daniel_silva37@estudante.sesisenai.org.br>
2023-07-26 20:08:19 -0300

    Aula01 inicio

---
+++ Aula01/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    /**
+     * Método de entrada da aplicação.
+     * @retorno vazio
+     * @autor Daniel
+     **/
+    public static void main(String[] args) {
+        System.out.println("Alô mamãe!");
+
+        /*
+        * O que precisamos rever?
+        *
+        * Declaração, inicialização e utilizaão de variáveis
+        * tipos de Dados
+        * Operadores matemáticos
+        * Operadores compostos
+        * Operadores de comparação
+        * Operadores lógicos
+        * intrução if/else
+        * instrução switch/case
+        * opedador ternário
+        * operadores unitários
+        *
+        */
+    }
+}
