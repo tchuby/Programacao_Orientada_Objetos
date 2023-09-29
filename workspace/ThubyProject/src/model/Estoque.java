082e9b2349c9738f5a48f2ae7f501cf8ea53e0da
DANIEL ANDRADE SILVA <daniel_silva37@estudante.sesisenai.org.br>
2023-09-29 19:23:47 -0300

    Iniciando a classe Estoque

--- workspace/ThubyProject/src/model/Estoque.java
+++ workspace/ThubyProject/src/model/Estoque.java
@@ -1,5 +1,15 @@
 package model;
 
-public class Estoque {
+import java.util.ArrayList;
 
+public class Estoque {
+	private ArrayList<Produto> estoque;
+	
+	public Estoque() {
+		this.estoque = new ArrayList<>();
+	}
+	
+	public ArrayList<Produto> getEstoque(){
+		return this.estoque;
+	}
 }
